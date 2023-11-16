889c1625b9541c683b966e890c806f437f7c2d18
Anton Shiryaev <med.phisiker@gmail.com>
2023-11-16 14:15:51 +0000

    feat(datasets): Написать скрипт для конвертации датасета RTSD
    Написать скрипт для конвертации датасета RTSD

---
+++ GUN_dataset_fiftyone_YOLOv5_dataset_convert.py
@@ -0,0 +1,26 @@
+import fiftyone as fo
+import os
+
+name = "RSTD"
+dataset_dir = "data/archive"
+
+# Load the dataset, using tags to mark the samples in each split
+dataset = fo.Dataset(name)
+
+dataset.add_dir(
+    dataset_dir=dataset_dir,
+    dataset_type=fo.types.COCODetectionDataset,
+    tags="train",
+)
+
+# View summary info about the dataset
+print(dataset)
+
+# Print the first few samples in the dataset
+print(dataset.head())
+
+# Export the dataset
+dataset.export(
+    export_dir="cvat",
+    dataset_type=fo.types.CVATImageDataset
+)
