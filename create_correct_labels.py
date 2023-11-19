849f6c1abf730d0f363ce50951b32ef80d6bb70a
Anton Shiryaev <med.phisiker@gmail.com>
2023-11-19 19:04:24 +1000

    feat(dataset): Осуществить конвертацию labels RTSD -> yolo format
    Осуществить конвертацию labels RTSD -> yolo format. FiftyOne и Datumaro
    считывают неправильное колчиество классов из датасета.

---
+++ create_correct_labels.py
@@ -0,0 +1,31 @@
+import json
+import yaml
+
+
+def read_json(filename: str):
+    with open(filename) as f_in:
+        dct_data = json.load(f_in)
+
+    return dct_data
+
+
+if __name__ == "__main__":
+    input_rtsd_json_labels_path = "./data/label_map.json"
+    output_yolo_yaml_path = "./data/yolo/dataset.yaml"
+
+    label_to_num_mapping = read_json(input_rtsd_json_labels_path)
+
+    # создаем словарь для yaml опысывающего датасет в формате
+    # YOLO-Ultralytics
+    # (https://openvinotoolkit.github.io/datumaro/stable/docs/data-formats/formats/yolo_ultralytics.html)
+    yolo_yaml = {}
+    yolo_yaml["train"] = "./images/train"
+    yolo_yaml["val"] = "./images/val"
+    yolo_yaml["names"] = {}
+    for val, key in enumerate(label_to_num_mapping):
+        yolo_yaml["names"].update({val: key})
+
+    # записываем словарь в yaml-файлF
+    yaml_string = yaml.dump(yolo_yaml)
+    with open(output_yolo_yaml_path, "w") as file:
+        file.write(yaml_string)
