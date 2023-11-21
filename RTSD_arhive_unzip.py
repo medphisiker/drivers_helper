3e915c48c25c732f8843fb23a802556ddfddec5d
Anton Shiryaev <med.phisiker@gmail.com>
2023-11-22 04:15:00 +1000

    feat(dataset): написать скрипт для разархивирования датасета RTSD
    написать скрипт для разархивирования датасета RTSD

---
+++ RTSD_arhive_unzip.py
@@ -0,0 +1,21 @@
+import zipfile
+import shutil
+import os
+
+
+if __name__ == "__main__":
+    # zip arhive of RSTD Dataset
+    zip_path = "data/archive.zip"
+    dest_dir_path = "data/RTSD"
+
+    # MS COCO FiftyOne structure
+    frames_dir_src = "rtsd-frames"
+    frames_dir = "data"
+
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(dest_dir_path)
+
+    frames_dir_src = os.path.join(dest_dir_path, frames_dir_src)
+    dst_dir_src = os.path.join(dest_dir_path, frames_dir)
+        
+    os.rename(frames_dir_src, dst_dir_src)
