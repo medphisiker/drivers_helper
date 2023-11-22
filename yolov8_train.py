0d96a584de47d9131d268655d8b3c86d04e0bab4
Anton Shiryaev <med.phisiker@gmail.com>
2023-11-22 22:06:49 +1000

    feat(train model): указать путь к фильтрованному датасету
    указать путь к фильтрованному датасету

--- yolov8_train.py
+++ yolov8_train.py
@@ -3,9 +3,9 @@ from ultralytics import YOLO
 model = YOLO("yolov8s.pt")
 
 res = model.train(
-    data="data/yolo/dataset.yaml",
-    epochs=80,
-    batch=64,
+    data="data/RSTD_filtered_yolov5/dataset.yaml",
+    epochs=20,
+    batch=32,
     workers=12,
     patience=5,
     save_period=2,
