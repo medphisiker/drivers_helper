d0ffa2fb599b903dbda667f1830949ac7a3c70d3
Anton Shiryaev <med.phisiker@gmail.com>
2023-11-22 20:31:43 +1000

    feat(data utils): использовать функции из модуля date data_utils
    использовать функции из модуля date data_utils

--- images_folder_arhivier.py
+++ images_folder_arhivier.py
@@ -1,18 +1,10 @@
 import os
 import zipfile
 
-
-def zip_directory(folder_path, zip_file):
-    for folder_name, subfolders, filenames in os.walk(folder_path):
-        for filename in filenames:
-            # Create complete filepath of file in directory
-            file_path = os.path.join(folder_name, filename)
-            # Add file to zip
-            zip_file.write(file_path)
-
+from data_utils import zip_directory
 
 if __name__ == "__main__":
-    dir_path = "data/RSTD_cvat_train/data"
+    dir_path = "data/RTSD_val_cvat_filtered/data"
 
     archive_path = dir_path + ".zip"
     zip_file = zipfile.ZipFile(archive_path, "w")
