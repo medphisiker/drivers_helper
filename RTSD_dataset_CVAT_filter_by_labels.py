e5ca7166ebf35a080bf1458260b86b41b2e0dbdf
Anton Shiryaev <med.phisiker@gmail.com>
2023-11-22 20:30:52 +1000

    feat(data utlis): использовать функции из модуля date data_utils
    использовать функции из модуля date data_utils

--- RTSD_dataset_CVAT_filter_by_labels.py
+++ RTSD_dataset_CVAT_filter_by_labels.py
@@ -1,60 +1,40 @@
 import fiftyone as fo
 from fiftyone import ViewField as F
-import os
+from data_utils import read_classes_txt
 
-name = "RTSD"
-dataset_dir = "data/rstd"
-include_classes = (
-    "5_19_1",
-    "2_1",
-    "5_16",
-    "5_15_2",
-    "3_24",
-    "2_4",
-    "3_27",
-    "1_23",
-    "4_1_1",
-    "5_20",
-    "3_20",
-    "5_15_3",
-    "5_15_1",
-    "5_15_2_2",
-    "1_17",
-    "4_2_3",
-    "5_15_5",
-    "4_2_1",
-    "4_1_4",
-    "7_3",
-    "6_4",
-    "6_16",
-    "5_5",
-    "1_25",
-    "8_2_1",
-    "2_3_2",
-    "3_1",
-)
 
-# Load the dataset, using tags to mark the samples in each split
-dataset = fo.Dataset(name)
+if __name__ == "__main__":
+    name = "RTSD"
+    dataset_dir = "data/RSTD_cvat_train"
+    classes_txt_path = "include_classes.txt"
+    export_dir_name = "data/RTSD_train_cvat_filtered"
 
-dataset.add_dir(
-    dataset_dir=dataset_dir,
-    dataset_type=fo.types.COCODetectionDataset,
-    tags="train",
-)
+    include_classes = read_classes_txt(classes_txt_path)
 
-# View summary info about the dataset
-print(dataset)
+    # Load the dataset, using tags to mark the samples in each split
+    dataset = fo.Dataset(name)
 
-# Print the first few samples in the dataset
-print(dataset.head())
+    dataset.add_dir(
+        dataset_dir=dataset_dir,
+        dataset_type=fo.types.CVATImageDataset,
+        tags="val",
+    )
 
-# Populate metadata on all samples
-dataset.compute_metadata()
+    # View summary info about the dataset
+    print(dataset)
 
-# Фильтруем датасет по классам
-filtered_view = dataset.filter_labels("detections", F("label").is_in(include_classes)
-)
+    # Print the first few samples in the dataset
+    print(dataset.head())
 
-# Export the dataset
-filtered_view.export(export_dir="RTSD_val_cvat_filtered", dataset_type=fo.types.CVATImageDataset)
+    # Populate metadata on all samples
+    dataset.compute_metadata()
+
+    # Фильтруем датасет по классам
+    filtered_view = dataset.filter_labels(
+        "detections", F("label").is_in(include_classes)
+    )
+
+    # Export the dataset
+    filtered_view.export(
+        export_dir=export_dir_name, dataset_type=fo.types.CVATImageDataset
+    )
--- create_labels_for_CVAT_json.py
+++ create_labels_for_CVAT_json.py
@@ -1,37 +1,12 @@
 import json
+from data_utils import read_classes_txt
 
 
 if __name__ == "__main__":
-    include_classes = (
-        "5_19_1",
-        "2_1",
-        "5_16",
-        "5_15_2",
-        "3_24",
-        "2_4",
-        "3_27",
-        "1_23",
-        "4_1_1",
-        "5_20",
-        "3_20",
-        "5_15_3",
-        "5_15_1",
-        "5_15_2_2",
-        "1_17",
-        "4_2_3",
-        "5_15_5",
-        "4_2_1",
-        "4_1_4",
-        "7_3",
-        "6_4",
-        "6_16",
-        "5_5",
-        "1_25",
-        "8_2_1",
-        "2_3_2",
-        "3_1",
-    )
-    output_cvat_json_path = "./data/RTSD_val_cvat_filtered/labels.json"
+    classes_txt_path = "include_classes.txt"
+    output_cvat_json_path = "data/RTSD_train_cvat_filtered/labels.json"
+
+    include_classes = read_classes_txt(classes_txt_path)
 
     # создаем словарь для CVAT опысывающего классы
     cvat_json = []
