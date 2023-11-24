872c777bbcbae2d718aa4c45d4d7f1d2c4c8e8f4
GrgBekh <gbeh@list.ru>
2023-11-24 20:31:26 +0500

    adding inference class

---
+++ OVino_inf_class.py
@@ -0,0 +1,80 @@
+from ultralytics import YOLO
+import numpy as np
+import pandas as pd
+import cv2
+
+class OVino_YOLO_inference_class():
+
+    def __init__(self, model_path, task='detect'):
+        self.net = YOLO(model_path, task=task)
+
+    def predict_on_image(self, img):
+        return self.net.predict(img)
+
+    def predict_and_draw_image(self, img):
+        res = self.net.predict(img)
+        some_frame = cv2.imread(img)
+
+        for result in res:
+            boxes = result.boxes.cpu().numpy()
+            xyxys = boxes.xyxy
+            for xyxy in xyxys:
+                cv2.rectangle(some_frame, (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3])), (255, 0, 0), 3)
+
+        cv2.imshow('img', some_frame)
+
+        while (True):
+            k = cv2.waitKey(0)
+            if k == -1:  # if no key was pressed, -1 is returned
+                continue
+            else:
+                break
+        cv2.destroyWindow('img')
+
+    def predict_on_video(self, video):
+        return self.net.predict(video)
+
+    def predict_and_display_video(self, video, frame_stride=10):
+
+        cap = cv2.VideoCapture(video)
+        counter = 0
+        boxes = []
+        xyxys = []
+        res = []
+
+        while True:
+            ret, frame = cap.read()
+
+            if counter % frame_stride == 0:
+                res = self.net.predict(frame, verbose=False)
+
+            for result in res:
+                boxes = result.boxes.cpu().numpy()
+                xyxys = boxes.xyxy
+                for xyxy in xyxys:
+                    cv2.rectangle(frame, (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3])), (255, 0, 0), 3)
+
+            cv2.imshow('video feed', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+    @staticmethod
+    def parse_video_detections(detection_results):
+        dict_of_frames_detections = {}
+        frame_counter = 0
+        classes_names = detection_results[0].names
+        for result in detection_results:
+            dict_of_frames_detections[f'frame_{frame_counter}'] = {}
+            detected_classes_instances = result.boxes.data[:, 5].to(int)
+            for class_instance_label in detected_classes_instances:
+                if result.names[int(class_instance_label)] in dict_of_frames_detections[
+                    f'frame_{frame_counter}'].keys():
+                    dict_of_frames_detections[f'frame_{frame_counter}'][result.names[int(class_instance_label)]] += 1
+                else:
+                    dict_of_frames_detections[f'frame_{frame_counter}'][result.names[int(class_instance_label)]] = 1
+            frame_counter += 1
+        return dict_of_frames_detections
