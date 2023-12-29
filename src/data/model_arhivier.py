03bbab706af633a3e54b22bba89b555b66e73530
Anton Shiryaev <med.phisiker@gmail.com>
2023-12-29 21:55:17 +1000

    refactor(move): Move model_arhivier.py to src/data
    Move model_arhivier.py to src/data

--- src/data/model_arhivier.py
+++ src/data/model_arhivier.py
@@ -6,8 +6,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", "data"))
 from data_utils import zip_directory
 
 if __name__ == "__main__":
-    dir_path = "models/train"
-    archive_path = "yolov8s_with_metrics.zip"
+    dir_path = "models/models/yolov8s_640"
+    archive_path = "yolov8s_640.zip"
 
     zip_file = zipfile.ZipFile(archive_path, "w")
     zip_directory(dir_path, zip_file)
